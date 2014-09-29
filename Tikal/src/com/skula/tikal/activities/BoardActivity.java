d0f2db51f38922eefe46c78488a9d4bbeebe1799
slown <slown@Nemesis>
2014-09-29 21:11:18 +0200

    first init

---
+++ Tikal/src/com/skula/tikal/activities/BoardActivity.java
@@ -0,0 +1,15 @@
+package com.skula.tikal.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.skula.tikal.R;
+
+public class BoardActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.boardlayer);
+	}
+}
